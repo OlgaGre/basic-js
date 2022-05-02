2f1a2ed423b1e010e1029d07c3aa53ecf59a1194
OlgaGre <grechanyuko@gmail.com>
2022-05-02 11:45:50 +0300

    feat: cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,16 +20,82 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+
+    encrypt(message = false, key = false) {
+        if (message == false || key == false) {
+            throw new Error('Incorrect arguments!');
+        } else {
+            let alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            message = message.toUpperCase();
+            key = key.toUpperCase();
+            while (key.length < message.length) { // удлиняем ключ до сообщения
+                key += key
+            }
+            let i = 0;
+            let j = 0;
+            let result = ''
+            while (i < message.length && j < key.length) {
+
+                if (alpha.includes(message[i])) {
+                    let indexInAlpha = alpha.indexOf(message[i]) //0
+                    let indexInKey = alpha.indexOf(key[j]) //0
+                    result += alpha[indexInKey + indexInAlpha]
+                    i++
+                    j++
+                } else {
+                    //если не латинская буква то добавляем, что есть
+
+                    result += message[i]
+                    i++
+                }
+            }
+            return result
+        }
+    }
+    decrypt(encryptedMessage = false, key = false) {
+        if (encryptedMessage == false || key == false) {
+            throw new Error('Incorrect arguments!');
+        } else {
+            // this.encrypt('attack at dawn!', 'alphonse').split('').reverse().join('')
+            let alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+            let revAlpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'].reverse()
+            encryptedMessage = encryptedMessage.toUpperCase();
+            key = key.toUpperCase();
+            while (key.length < encryptedMessage.length) { // удлиняем ключ до сообщения
+                key += key
+            }
+            let i = 0;
+            let j = 0;
+            let result = ''
+            while (i < encryptedMessage.length && j < key.length) {
+
+                if (alpha.includes(encryptedMessage[i])) {
+                    let indexInAlpha = alpha.indexOf(encryptedMessage[i], 25) //0
+                    let indexInKey = alpha.indexOf(key[j], 25) //0
+                    if (indexInAlpha - indexInKey >= 0) {
+                        result += alpha[indexInAlpha - indexInKey]
+                    } else {
+                        result += revAlpha[Math.abs(indexInAlpha - indexInKey + 1)]
+                    }
+
+
+                    //    result += alpha[indexInAlpha - indexInKey]
+                    i++
+                    j++
+                } else {
+                    //если не латинская буква то добавляем, что есть
+
+                    result += encryptedMessage[i]
+                    i++
+                }
+            }
+            return result
+                //   return result.split('').reverse().join('')
+        }
+    }
 }
 
+
 module.exports = {
-  VigenereCipheringMachine
+    VigenereCipheringMachine
 };
